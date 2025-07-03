cf663f01f94af6edacce6de93230bc2198699e52
JorgeGV <jorgegalvez1694@gmail.com>
2025-07-03 10:24:47 +1000

    update stress test to use flop rate object

--- examples/stress_test/main.f90
+++ examples/stress_test/main.f90
@@ -2,12 +2,16 @@ program main
    use pic_string_utils
    use pic_types
    use pic_timers
+   use pic_flop_recorder
+   use pic_flop_rate
    use mpi_f08
    implicit none
    integer(default_int) :: ierr, rank, size, ival
    real(dp), dimension(:), allocatable :: C_flat
-   integer(default_int) :: n, m, k, flat_size
+   !integer(default_int) :: n, m, k, flat_size
    integer(int64) :: flops, total_flops
+   !type(flop_recorder_type) :: pic_flops
+   type(flop_rate_type) :: pic_flops
    real(dp) :: max_time
    real(dp) :: elapsed_time
    real(dp) :: flop_rate
@@ -27,19 +31,23 @@ program main
       integer, parameter :: m = m_size
       integer, parameter :: n = m_size
       integer, parameter :: k = m_size
+      integer(int64) :: curr_flops
       integer :: i
       allocate (A(n, k), B(k, m), C(n, m))
       A = 1.0_dp
       B = 1.0_dp
       C = 0.0_dp
       call dgemm('N', 'N', n, m, k, 1.0d0, A, n, B, k, 0.0d0, C, n)
-      call my_timer%start()
+      !call my_timer%start()
+      call pic_flops%start_time()
       do i = 1, n_loops
-         flops = flops + 2*m*n*k
+         curr_flops = 2*m*n*k
+         call pic_flops%add_flops(curr_flops)
          call dgemm('N', 'N', n, m, k, 1.0d0, A, n, B, k, 0.0d0, C, n)
       end do
-      call my_timer%end()
-      elapsed_time = my_timer%get_elapsed_time()
+      call pic_flops%stop_time()
+      elapsed_time = pic_flops%get_time()
+      flops = pic_flops%get_flops()
       !print *, "TIME is " // to_string(elapsed_time) // " seconds in rank "//to_string(rank)
 
    end block
@@ -49,7 +57,7 @@ program main
    call MPI_Reduce(elapsed_time, max_time, 1, MPI_DOUBLE_PRECISION, MPI_MAX, 0, MPI_COMM_WORLD, ierr)
 
    if (rank == 0) then
-      flop_rate = real(total_flops, dp)/max_time/1.0d9
+      flop_rate = pic_flops%get_flop_rate()!real(total_flops, dp)/max_time/1.0d9
       print *, "Global time: ", max_time, " seconds"
       print *, "Total GFLOPs: ", total_flops/1d9
       print *, "Global FLOP rate: ", flop_rate, " GFLOP/s"
