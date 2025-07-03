b8793a68169264bd3d3e5f8b84e3772e44d59c9e
JorgeG94 <JorgeG94@users.noreply.github.com>
2025-07-03 13:00:28 +0000

    deploy: b4884882325845a5286e75e48f9b2bdd287b9183

--- src/pic.f90
+++ src/pic.f90
@@ -1,5 +1,14 @@
 !! the main pic module interface: versions, banners, random quotes etc will go here
 module pic
+   use pic_types
+   use pic_debugging_tools
+   use pic_mpi
+   use pic_timers
+   use pic_flop_recorder
+   use pic_flop_rate
+   use pic_logger
+   use pic_global_definitions
+   use pic_command_line
  !! simple interface module that prints banner and other information about the library
  !! mostly here to verify installs, etc.
    implicit none
