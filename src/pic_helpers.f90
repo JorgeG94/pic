4379a4010970d8fdbf1df03a25355a95d097c58d
JorgeG94 <JorgeG94@users.noreply.github.com>
2025-07-22 13:10:29 +0000

    deploy: 787c591a8a040ec3fc41c66b56ec870245a8ffb5

---
+++ src/pic_helpers.f90
@@ -0,0 +1,30 @@
+module pic_test_helpers
+   use pic_types, only: int64, dp
+   implicit none
+
+   private
+   public :: dummy_work
+
+contains
+   subroutine dummy_work()
+      integer(int64) ::  i, j, k
+      integer :: m
+      real(dp), allocatable :: A(:, :), B(:, :), C(:, :)
+
+      m = 256
+      allocate (A(m, m), B(m, m), C(m, m))
+      A = 1.0_dp
+      B = 1.0_dp
+      C = 0.0_dp
+      do i = 1, m
+         do j = 1, m
+            do k = 1, m
+
+               C(i, j) = C(i, j) + A(i, k)*B(k, j)
+            end do
+         end do
+      end do
+      print *, C(1, 1)
+      deallocate (A, B, C)
+   end subroutine dummy_work
+end module pic_test_helpers
