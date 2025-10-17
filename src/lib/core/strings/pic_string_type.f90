dce10c4913c2b8b1b489491d6f5118cbdce39fb1
Jorge L GV <jorgegalvez1694@gmail.com>
2025-10-17 13:12:17 +1100

    more features and tests

--- src/lib/core/strings/pic_string_type.f90
+++ src/lib/core/strings/pic_string_type.f90
@@ -1,27 +1,51 @@
 module pic_string_mod
    use pic_types, only: int64
+   use pic_optional_value, only: pic_optional
    implicit none
    private
    public :: pic_string_type
+   public :: operator(==), operator(/=)
 
    type :: pic_string_type
       character(len=:), allocatable :: buf
       integer(int64) :: len = 0_int64
       integer(int64) :: cap = 0_int64
    contains
+      ! queries
       procedure :: size => pic_string_size
       procedure :: capacity => pic_string_capacity
       procedure :: empty => pic_string_empty
+      procedure :: starts_with => pic_string_starts_with
+      procedure :: ends_with => pic_string_ends_with
+      ! modifiers
       procedure :: clear => pic_string_clear
       procedure :: reserve => pic_string_reserve
       procedure :: assign => pic_string_assign
       procedure :: append => pic_string_append
       procedure :: push_back => pic_string_push_back
       procedure :: to_char => pic_string_to_char
+      ! trimming
+      procedure :: ltrim => pic_string_ltrim
+      procedure :: rtrim => pic_string_rtrim
+      procedure :: trim => pic_string_trim
+      ! search & slicing
+      procedure :: find => pic_string_find
+      procedure :: substr => pic_string_substr
    end type pic_string_type
 
    interface ensure_capacity_
       module procedure :: ensure_capacity
+   end interface
+   interface operator(==)
+      module procedure pic_string_eq_string    ! pic_string_type == pic_string_type
+      module procedure pic_string_eq_char      ! pic_string_type == character(*)
+      module procedure char_eq_pic_string      ! character(*)   == pic_string_type
+   end interface
+
+   interface operator(/=)
+      module procedure pic_string_ne_string
+      module procedure pic_string_ne_char
+      module procedure char_ne_pic_string
    end interface
 
 contains
@@ -113,5 +137,144 @@ contains
       character(len=self%len) :: out
       if (self%len > 0) out = self%buf(1:self%len)
    end function pic_string_to_char
+
+   !----------------- trimming -----------------
+   subroutine pic_string_rtrim(self)
+      class(pic_string_type), intent(inout) :: self
+      integer(int64) :: j
+      if (self%len == 0_int64) return
+      j = self%len
+      do
+         if (j < 1_int64) exit
+         if (self%buf(j:j) <= ' ') then
+            j = j - 1_int64
+         else
+            exit
+         end if
+      end do
+      self%len = max(0_int64, j)
+   end subroutine
+
+   subroutine pic_string_ltrim(self)
+      class(pic_string_type), intent(inout) :: self
+      integer(int64) :: i, n
+      if (self%len == 0_int64) return
+      i = 1_int64
+      do
+         if (i > self%len) exit
+         if (self%buf(i:i) <= ' ') then
+            i = i + 1_int64
+         else
+            exit
+         end if
+      end do
+      if (i > 1_int64) then
+         n = self%len - (i - 1_int64)
+         if (n > 0_int64) self%buf(1:n) = self%buf(i:self%len)
+         self%len = max(0_int64, n)
+      end if
+   end subroutine
+
+   subroutine pic_string_trim(self)
+      class(pic_string_type), intent(inout) :: self
+      call self%rtrim()
+      call self%ltrim()
+   end subroutine
+
+   !----------------- queries -----------------
+   pure logical function pic_string_starts_with(self, pat) result(ok)
+      class(pic_string_type), intent(in) :: self
+      character(*), intent(in) :: pat
+      integer :: m
+      m = len(pat)
+      ok = (self%len >= m .and. (m == 0 .or. self%buf(1:m) == pat))
+   end function
+
+   pure logical function pic_string_ends_with(self, pat) result(ok)
+      class(pic_string_type), intent(in) :: self
+      character(*), intent(in) :: pat
+      integer :: m
+      m = len(pat)
+      ok = (self%len >= m .and. (m == 0 .or. self%buf(self%len - m + 1:self%len) == pat))
+   end function
+
+   !----------------- search & slicing -----------------
+   pure integer(int64) function pic_string_find(self, pat, from) result(pos)
+      class(pic_string_type), intent(in) :: self
+      character(*), intent(in) :: pat
+      integer(int64), optional, intent(in):: from
+      integer(int64) :: i0
+      integer        :: k
+
+      if (self%len == 0_int64 .or. len(pat) == 0) then
+         pos = 0_int64; return
+      end if
+
+      i0 = pic_optional(from, 1_int64)   ! <- clean & safe
+
+      if (i0 < 1_int64 .or. i0 > self%len) then
+         pos = 0_int64; return
+      end if
+
+      k = index(self%buf(i0:self%len), pat)
+      if (k > 0) then
+         pos = int(k, int64) + i0 - 1_int64
+      else
+         pos = 0_int64
+      end if
+   end function
+
+   function pic_string_substr(self, i, n) result(out)
+      class(pic_string_type), intent(in) :: self
+      integer(int64), intent(in) :: i      ! 1-based
+      integer(int64), intent(in) :: n      ! length requested
+      type(pic_string_type) :: out
+      integer(int64) :: i2, n2
+      if (n <= 0_int64 .or. i > self%len) return
+      i2 = max(1_int64, i)
+      n2 = min(self%len - i2 + 1_int64, n)
+      call out%reserve(n2)
+      if (n2 > 0_int64) out%buf(1:n2) = self%buf(i2:i2 + n2 - 1_int64)
+      out%len = n2
+   end function
+
+   !----------------- equality operators -----------------
+   pure logical function pic_string_eq_string(a, b) result(ok)
+      class(pic_string_type), intent(in) :: a
+      class(pic_string_type), intent(in) :: b
+      ok = (a%len == b%len)
+      if (ok .and. a%len > 0_int64) ok = (a%buf(1:a%len) == b%buf(1:b%len))
+   end function
+
+   pure logical function pic_string_eq_char(a, c) result(ok)
+      class(pic_string_type), intent(in) :: a
+      character(*), intent(in) :: c
+      ok = (a%len == len(c))
+      if (ok .and. a%len > 0_int64) ok = (a%buf(1:a%len) == c)
+   end function
+
+   pure logical function char_eq_pic_string(c, a) result(ok)
+      character(*), intent(in) :: c
+      class(pic_string_type), intent(in) :: a
+      ok = (a == c)
+   end function
+
+   pure logical function pic_string_ne_string(a, b) result(ok)
+      class(pic_string_type), intent(in) :: a
+      class(pic_string_type), intent(in) :: b
+      ok = .not. (a == b)
+   end function
+
+   pure logical function pic_string_ne_char(a, c) result(ok)
+      class(pic_string_type), intent(in) :: a
+      character(*), intent(in) :: c
+      ok = .not. (a == c)
+   end function
+
+   pure logical function char_ne_pic_string(c, a) result(ok)
+      character(*), intent(in) :: c
+      class(pic_string_type), intent(in) :: a
+      ok = .not. (a == c)
+   end function
 
 end module pic_string_mod
--- test/test_pic_string_type.f90
+++ test/test_pic_string_type.f90
@@ -1,7 +1,7 @@
 module test_pic_string_type
    use testdrive, only: new_unittest, unittest_type, error_type, check
-   use pic_string_mod, only: pic_string_type
-   use pic_types, only: int64
+   use pic_string_mod, only: pic_string_type, operator(==), operator(/=)
+   use pic_types, only: int64, dp
    implicit none
    private
 
@@ -20,7 +20,16 @@ contains
                    new_unittest("test_pic_string_type_push_back_chars", test_pic_string_type_push_back_chars), &
                    new_unittest("test_pic_string_type_reserve_growth", test_pic_string_type_reserve_growth), &
                    new_unittest("test_pic_string_type_clear_keeps_cap", test_pic_string_type_clear_keeps_cap), &
-                   new_unittest("test_pic_string_type_to_char_roundtrip", test_pic_string_type_to_char_roundtrip) &
+                   new_unittest("test_pic_string_type_to_char_roundtrip", test_pic_string_type_to_char_roundtrip), &
+                   new_unittest("test_pic_string_type_trim_basic", test_pic_string_type_trim_basic), &
+                   new_unittest("test_pic_string_type_trim_edge_cases", test_pic_string_type_trim_edge_cases), &
+                   new_unittest("test_pic_string_type_starts_ends", test_pic_string_type_starts_ends), &
+                   new_unittest("test_pic_string_type_find_basic", test_pic_string_type_find_basic), &
+                   new_unittest("test_pic_string_type_find_with_from", test_pic_string_type_find_with_from), &
+                   new_unittest("test_pic_string_type_find_not_found", test_pic_string_type_find_not_found), &
+                   new_unittest("test_pic_string_type_substr_basic", test_pic_string_type_substr_basic), &
+                   new_unittest("test_pic_string_type_substr_bounds", test_pic_string_type_substr_bounds), &
+                   new_unittest("test_pic_string_type_equality_ops", test_pic_string_type_equality_ops) &
                    ]
    end subroutine collect_pic_string_type_tests
 
@@ -150,5 +159,134 @@ contains
       c = s%to_char()
       call check(error, c == "roundtrip", "to_char returns identical contents")
    end subroutine test_pic_string_type_to_char_roundtrip
+
+   subroutine test_pic_string_type_trim_basic(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      call s%assign("  abc  ")
+      call s%ltrim()
+      call check(error, s%to_char() == "abc  ", "ltrim failed")
+      if (allocated(error)) return
+      call s%assign("  abc  ")
+      call s%rtrim()
+      call check(error, s%to_char() == "  abc", "rtrim failed")
+      if (allocated(error)) return
+      call s%assign("  abc  ")
+      call s%trim()
+      call check(error, s%to_char() == "abc", "trim failed")
+   end subroutine test_pic_string_type_trim_basic
+
+   subroutine test_pic_string_type_trim_edge_cases(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      ! empty stays empty
+      call s%assign("")
+      call s%trim()
+      call check(error, s%size() == 0, "trim(empty) should remain empty")
+      if (allocated(error)) return
+      ! all spaces -> empty
+      call s%assign("     ")
+      call s%trim()
+      call check(error, s%size() == 0, "trim(all-spaces) should become empty")
+   end subroutine test_pic_string_type_trim_edge_cases
+
+   subroutine test_pic_string_type_starts_ends(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      call s%assign("foobar")
+      call check(error, s%starts_with("foo"), "starts_with('foo') should be true")
+      if (allocated(error)) return
+      call check(error, s%ends_with("bar"), "ends_with('bar') should be true")
+      if (allocated(error)) return
+      call check(error, s%starts_with("foobar"), "starts_with equal whole string")
+      if (allocated(error)) return
+      call check(error, s%ends_with("foobar"), "ends_with equal whole string")
+      if (allocated(error)) return
+      ! empty pattern is always true
+      call check(error, s%starts_with(""), "starts_with('') should be true")
+      if (allocated(error)) return
+      call check(error, s%ends_with(""), "ends_with('') should be true")
+      if (allocated(error)) return
+      ! negatives
+      call check(error,.not. s%starts_with("bar"), "starts_with('bar') false")
+      if (allocated(error)) return
+      call check(error,.not. s%ends_with("foo"), "ends_with('foo') false")
+   end subroutine test_pic_string_type_starts_ends
+
+   subroutine test_pic_string_type_find_basic(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      integer(kind=dp) :: p
+      call s%assign("abracadabra")
+      p = s%find("ra")
+      call check(error, p == 3_dp, "find('ra') should be 3")
+      if (allocated(error)) return
+      p = s%find("abra")     ! first occurrence
+      call check(error, p == 1_dp, "find('abra') should be 1")
+   end subroutine test_pic_string_type_find_basic
+
+   subroutine test_pic_string_type_find_with_from(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      integer(kind=dp) :: p
+      call s%assign("abracadabra")
+      p = s%find("ra", from=4_dp)   ! next 'ra' after index 4
+      print *, "DEBUG: p =", p
+      call check(error, p == 10_dp, "find('ra', from=4) should be 10")
+      if (allocated(error)) return
+      p = s%find("abra", from=2_dp)
+      call check(error, p == 8_dp, "find('abra', from=2) should be 8")
+   end subroutine test_pic_string_type_find_with_from
+
+   subroutine test_pic_string_type_find_not_found(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s
+      integer(kind=dp) :: p
+      call s%assign("hello")
+      p = s%find("xyz")
+      call check(error, p == 0_dp, "not found should be 0")
+      if (allocated(error)) return
+      p = s%find("", from=1_dp)     ! empty pattern => our API returns 0
+      call check(error, p == 0_dp, "empty pattern returns 0 by design")
+   end subroutine test_pic_string_type_find_not_found
+
+   subroutine test_pic_string_type_substr_basic(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s, t
+      call s%assign("abcdefg")
+      t = s%substr(4_dp, 3_dp)  ! 1-based: 'd','e','f'
+      call check(error, t%to_char() == "def", "substr(4,3) should be 'def'")
+   end subroutine test_pic_string_type_substr_basic
+
+   subroutine test_pic_string_type_substr_bounds(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: s, t
+      call s%assign("abcdefg")
+      t = s%substr(100_dp, 5_dp)
+      call check(error, t%size() == 0, "substr out-of-range should be empty")
+      if (allocated(error)) return
+      t = s%substr(3_dp, 0_dp)
+      call check(error, t%size() == 0, "substr with length 0 should be empty")
+      if (allocated(error)) return
+      t = s%substr(6_dp, 10_dp)
+      call check(error, t%to_char() == "fg", "substr clamps to end")
+   end subroutine test_pic_string_type_substr_bounds
+
+   subroutine test_pic_string_type_equality_ops(error)
+      type(error_type), allocatable, intent(out) :: error
+      type(pic_string_type) :: a, b
+      call a%assign("foo")
+      call b%assign("foo")
+      call check(error, a == b, "a == b should be true")
+      if (allocated(error)) return
+      call check(error, a == "foo", "a == 'foo' should be true")
+      if (allocated(error)) return
+      call check(error, "foo" == a, "'foo' == a should be true")
+      if (allocated(error)) return
+      call b%append("bar")
+      call check(error, a /= b, "a /= b after change")
+      if (allocated(error)) return
+      call check(error, a /= "foobar", "a /= 'foobar' should be true")
+   end subroutine test_pic_string_type_equality_ops
 
 end module test_pic_string_type
