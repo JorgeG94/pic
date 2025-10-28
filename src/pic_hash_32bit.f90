fb48f92235775763c8acc1c3d13a5a96295dfd91
JorgeG94 <JorgeG94@users.noreply.github.com>
2025-10-28 10:38:06 +0000

    deploy: 592caac28192428776fe4610245e4e41280c788c

---
+++ src/pic_hash_32bit.f90
@@ -0,0 +1,23 @@
+
+module pic_hash_32bit
+
+   use pic_hash_32bit_fnv, only: fnv_1_hash, fnv_1a_hash
+   use pic_types, only: &
+      dp, &
+      int32, &
+      int64
+
+   implicit none
+
+   private
+
+! pow32_over_phi is the odd integer that most closely approximates 2**32/phi,
+! where phi is the golden ratio 1.618...
+   integer(int32), parameter :: &
+      pow32_over_phi = int(z'9E3779B9', int32)
+
+   public :: &
+      fnv_1_hash, &
+      fnv_1a_hash
+
+end module pic_hash_32bit
